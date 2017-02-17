fc7a044f974c1b83cc0f7aa4ecffde5555976f27
Unknown <migu27studios@gmail.com>
2017-02-17 10:53:20 -0700

    Added Key Commands and Button Input

---
+++ New Unity Project/Assets/ButtonAndMouseInput.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonAndMouseInput : MonoBehaviour {
+
+    public void MyMouseClick ()
+    {
+        print("I clicked the button!");
+    }
+}
---
+++ New Unity Project/Assets/KeyInput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyInput : MonoBehaviour {
+
+   void start ()
+    {
+
+    }
+	
+	// Update is called once per frame.     Keep Update for a MINIMUM.
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("I hit A.");
+        }
+       /* else 
+        {
+            print("No input");
+        } */
+	}
+}
